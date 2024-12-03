0dff91016af39faad2d67c243663190b14d479d4
vapacity <131332667+vapacity@users.noreply.github.com>
2024-12-04 00:29:04 +0800

    init

---
+++ src/main/java/com/javaee/elderlycanteen/ElderlyCanteenApplication.java
@@ -0,0 +1,13 @@
+package com.javaee.elderlycanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderlyCanteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElderlyCanteenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaee/elderlycanteen/ElderlyCanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee.elderlycanteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElderlyCanteenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

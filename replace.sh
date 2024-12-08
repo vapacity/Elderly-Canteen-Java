aa9bc5951465397d50c1ca783859036af29c6114
YangRuakaka <141391836+YangRuakaka@users.noreply.github.com>
2024-12-08 15:56:14 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+# replace.sh
+# 判断指定路径下的jar包是否存在
+# 如果存在，更名备份
+# 将Jenkins打包编译后的项目移动到指定的项目目录下
 
-#replate.sh
-#判断指定路径下的jar包是否存在
-#如果存在，更名备份
-#将Jenkins打包编译后的项目移动到指定的项目目录下
-file="/java/project/my-spring-boot-app-1.0.0.jar"
-if [ -f "$file" ]
-then
-   mv /java/project/my-spring-boot-app-1.0.0.jar /java/project/my-spring-boot-app-1.0.0.jar.`date +%Y%m%d%H%M%S`
+file="/java/project/编译后包的名称.jar"
+
+# 确保目标目录存在
+if [ ! -d "/java/project" ]; then
+    echo "Directory /java/project does not exist, creating it."
+    mkdir -p /java/project
+    chown -R jenkins:jenkins /java/project
+    chmod -R 755 /java/project
 fi
-   mv /root/.jenkins/workspace/Elderly-Canteen-Java/target/my-spring-boot-app-1.0.0.jar /java/project/
 
+if [ -f "$file" ]; then
+    mv /java/project/编译后包的名称.jar /java/project/编译后包的名称.jar.`date +%Y%m%d%H%M%S`
+fi
+mv /root/.jenkins/workspace/项目名称/target/编译后包的名称.jar /java/project/

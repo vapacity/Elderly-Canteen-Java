9e27332271af21203d72be893b883e1a4d2b596b
vapacity <131332667+vapacity@users.noreply.github.com>
2024-12-13 13:12:19 +0800

    Ingredient related apis

--- src/main/java/com/javaee/elderlycanteen/dto/ingredient/IngreRequestDto.java
+++ src/main/java/com/javaee/elderlycanteen/dto/ingredient/IngreRequestDto.java
@@ -1,12 +1,15 @@
 package com.javaee.elderlycanteen.dto.ingredient;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class IngreRequestDto {
     @JsonProperty("ingredientId")
-    private String ingredientId;
+    @Schema(description = "食材ID", example = "1")
+    private Integer ingredientId;
 
     @JsonProperty("ingredientName")
+    @Schema(description = "食材名稱", example = "·1")
     private String ingredientName;
 }

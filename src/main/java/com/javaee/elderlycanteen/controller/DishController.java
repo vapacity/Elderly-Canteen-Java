3fbe31124ef490f64fd80878293f0470e4cea6eb
vapacity <131332667+vapacity@users.noreply.github.com>
2024-12-13 20:02:11 +0800

    Add search dish api, integrated with mybatis xml method

--- src/main/java/com/javaee/elderlycanteen/controller/DishController.java
+++ src/main/java/com/javaee/elderlycanteen/controller/DishController.java
@@ -1,20 +1,35 @@
 package com.javaee.elderlycanteen.controller;
 
+import com.javaee.elderlycanteen.dto.dish.AllDishResponseDto;
+import com.javaee.elderlycanteen.minio.MinioService;
 import com.javaee.elderlycanteen.service.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dishes")
+@RequestMapping("api/Dish")
 public class DishController {
 
     private final DishService dishService;
+    private final MinioService minioService;
 
     @Autowired
-    public DishController(DishService dishService) {
+    public DishController(DishService dishService, MinioService minioService) {
         this.dishService = dishService;
+        this.minioService = minioService;
     }
 
     // 在这里添加特定的API端点方法
+    @GetMapping("/search")
+    public AllDishResponseDto searchDish(@RequestParam(value = "name", required = false) String name,
+                                         @RequestParam(value="category",required = false) String category){
+        return dishService.searchDish(name, category);
+    }
+
+//    @PostMapping("/addDish")
+//
+//    @PutMapping("/updateDish")
+//
+//    @DeleteMapping("/deleteDish/{dishId}")
+
 }
--- src/main/java/com/javaee/elderlycanteen/service/DishService.java
+++ src/main/java/com/javaee/elderlycanteen/service/DishService.java
@@ -2,9 +2,15 @@ package com.javaee.elderlycanteen.service;
 
 import com.javaee.elderlycanteen.dao.CategoryDao;
 import com.javaee.elderlycanteen.dao.DishDao;
+import com.javaee.elderlycanteen.dto.dish.AllDishResponseDto;
 import com.javaee.elderlycanteen.dto.dish.DishDto;
+import com.javaee.elderlycanteen.entity.Dish;
+import com.javaee.elderlycanteen.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DishService {
@@ -17,5 +23,28 @@ public class DishService {
         this.categoryDao = categoryDao;
     }
 
-    // 在这里添加特定的业务逻辑方法
+    public AllDishResponseDto searchDish(String name, String category) {
+        AllDishResponseDto allDishResponseDto = new AllDishResponseDto();
+        List<DishDto> dishDtoList = new ArrayList<DishDto>();
+        List<Dish> dishes = new ArrayList<Dish>();
+        if(name == null && category == null){
+            dishes = dishDao.getAllDish();
+        }
+        else{
+            dishes = dishDao.getDishesByNameAndCategoryVague(name, category);
+        }
+        if(dishes.size() == 0){
+            throw new NotFoundException("Dishes not found");
+        }
+        for(Dish dish : dishes){
+            DishDto dishDto = new DishDto();
+            dishDto = dishDao.getDishDetailsById(dish.getDishId());
+            dishDtoList.add(dishDto);
+        }
+        allDishResponseDto.setResponse(dishDtoList);
+        allDishResponseDto.setSuccess(true);
+        allDishResponseDto.setMsg("Success");
+        return allDishResponseDto;
+    }
+
 }

02fea3d6d3b62f2117cfd5005e238c0268931700
vapacity <131332667+vapacity@users.noreply.github.com>
2024-12-16 00:46:32 +0800

    update dish method

--- src/main/java/com/javaee/elderlycanteen/dto/dish/DishDto.java
+++ src/main/java/com/javaee/elderlycanteen/dto/dish/DishDto.java
@@ -1,16 +1,32 @@
 package com.javaee.elderlycanteen.dto.dish;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DishDto {
     @JsonProperty("dishId")
     private Integer dishId;
 
     @JsonProperty("dishName")
     private String dishName;
+
+    @JsonProperty("price")
+    private Integer price;
+
+    @JsonProperty("cateId")
+    private Integer cateId;
+
+    @JsonProperty("cateName")
+    private String cateName;
+
+    @JsonProperty("imageUrl")
+    private String imageUrl;
 
     @JsonProperty("formula")
     public List<FormulaDto> formula;

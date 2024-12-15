0f7ac8f8533f7503dbd1f3c655bf2cdf4b4a7513
12345zsk <129924442+12345zsk@users.noreply.github.com>
2024-12-15 21:19:25 +0800

    11

--- src/main/java/com/javaee/elderlycanteen/enumeration/FinanceTypeEnum.java
+++ src/main/java/com/javaee/elderlycanteen/enumeration/FinanceTypeEnum.java
@@ -1,4 +1,27 @@
 package com.javaee.elderlycanteen.enumeration;
 
 public enum FinanceTypeEnum {
+    ORDER("Order");
+
+    private String description;  // 英文描述
+
+    // 构造方法
+    FinanceTypeEnum(String description) {
+        this.description = description;
+    }
+
+    // 获取英文描述
+    public String getDescription() {
+        return description;
+    }
+
+    // 通过英文描述获取对应的 enum 值
+    public static FinanceTypeEnum fromDescriptionEn(String description) {
+        for (FinanceTypeEnum status : FinanceTypeEnum.values()) {
+            if (status.getDescription().equalsIgnoreCase(description)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected value: " + description);
+    }
 }

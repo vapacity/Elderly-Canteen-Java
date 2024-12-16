06817afb3b7367526b6a7dab4062ce29fa7003c9
YangRuakaka <141391836+YangRuakaka@users.noreply.github.com>
2024-12-16 17:40:00 +0800

    update
    update

--- src/main/java/com/javaee/elderlycanteen/controller/AccountController.java
+++ src/main/java/com/javaee/elderlycanteen/controller/AccountController.java
@@ -13,15 +13,21 @@ import com.javaee.elderlycanteen.exception.NotFoundException;
 import com.javaee.elderlycanteen.minio.MinioService;
 import com.javaee.elderlycanteen.service.AccountService;
 import com.javaee.elderlycanteen.utils.JWTUtils;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.util.ArrayList;
+
 import com.javaee.elderlycanteen.dto.login.LoginRequestIdDto;
+
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,7 +39,7 @@ public class AccountController {
     private final MinioService minioService;
 
     @Autowired
-    public AccountController(AccountService accountService,MinioService minioService) {
+    public AccountController(AccountService accountService, MinioService minioService) {
         this.accountService = accountService;
         this.minioService = minioService;
     }
@@ -97,9 +103,9 @@ public class AccountController {
     }
 
     @GetMapping("/getPersonInfo")
-    public PersonInfoResponseDto getPersonInfo(@RequestHeader(name="Authorization", required = false) String token) {
+    public PersonInfoResponseDto getPersonInfo(@RequestHeader(name = "Authorization", required = false) String token) {
         // 获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -111,12 +117,19 @@ public class AccountController {
     }
 
     @PostMapping("/alterPersonInfo")
-    public PersonInfoResponseDto alterPersonInfo(@RequestHeader(name="Authorization", required = false) String token,
-                                                 @RequestParam(value="avatar", required = false)MultipartFile avatar,
-                                                 @RequestBody PersonInfoRequestDto personInfo ) throws Exception {
+    public PersonInfoResponseDto alterPersonInfo(@RequestHeader(name = "Authorization", required = false) String token,
+                                                 @Parameter(name = "上传的图片", required = false)
+                                                 @RequestParam("avatar") MultipartFile avatar,
+                                                 @RequestParam(value = "accountName", required = false) String accountName,
+                                                 @RequestParam(value = "phoneNum", required = false) String phoneNum,
+                                                 @RequestParam(value = "gender", required = false) String gender,
+                                                 @RequestParam(value = "birthDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date birthDate,
+                                                 @RequestParam(value = "address", required = false) String address,
+                                                 @RequestParam(value = "name", required = false) String name
+    ) throws Exception {
 
         // 获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -124,14 +137,16 @@ public class AccountController {
         Integer accountId = tokenInfo.getAccountId();
         //把图片转换成url路径并上传到桶
         String fileName = avatar.getOriginalFilename();
-        minioService.uploadFile(fileName,avatar);
-        return accountService.alterPersonInfo(personInfo , accountId, fileName);
+        minioService.uploadFile(fileName, avatar);
+        //把用户信息封装成PersonInfoRequestDto对象
+        PersonInfoRequestDto personInfo = new PersonInfoRequestDto(accountName, phoneNum, gender, birthDate, address, name);
+        return accountService.alterPersonInfo(personInfo, accountId, fileName);
     }
 
     @GetMapping("/getAllAccount")
-    public AccountDto getAllAccount(@RequestHeader(name="Authorization", required = false) String token){
+    public AccountDto getAllAccount(@RequestHeader(name = "Authorization", required = false) String token) {
         // 获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -141,10 +156,10 @@ public class AccountController {
     }
 
     @PostMapping("/nameAuthenticate")
-    public AuthenticationResponseDto nameAuthenticate(@RequestHeader(name = "Authorization", required = false)String token,
-                                                     AuthenticationRequestDto input) {
+    public AuthenticationResponseDto nameAuthenticate(@RequestHeader(name = "Authorization", required = false) String token,
+                                                      AuthenticationRequestDto input) {
         //获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -156,9 +171,9 @@ public class AccountController {
 
     //修改密码
     @PostMapping("/changePassword")
-    public ResponseEntity<Void> changePassword(@RequestHeader(name = "Authorization", required = false)String token, String password) {
+    public ResponseEntity<Void> changePassword(@RequestHeader(name = "Authorization", required = false) String token, String password) {
         // 获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -169,36 +184,38 @@ public class AccountController {
 
     //更改密码
     @PostMapping("/alterPassword")
-    public ResponseEntity<Void> alterPassword(@RequestHeader(name = "Authorization", required = false)String token, PasswordRequestDto request) {
+    public ResponseEntity<Void> alterPassword(@RequestHeader(name = "Authorization", required = false) String token, PasswordRequestDto request) {
         // 获取accountId
-        if( token == null ){
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
         TokenInfo tokenInfo = JWTUtils.getTokenInfo(token);
         Integer accountId = tokenInfo.getAccountId();
-        boolean isOldPassword = accountService.VerifyPassword(request.getOldPassword(),accountId);
+        boolean isOldPassword = accountService.VerifyPassword(request.getOldPassword(), accountId);
         if (!isOldPassword) {
             throw new RuntimeException("原密码不正确");
         }
 
         return accountService.changePassword(accountId, request.getNewPassword());
     }
+
     //修改手机号
     @PostMapping("/changePhone")
-    public PhoneResponseDto changePhone(@RequestHeader(name = "Authorization", required = false) String token, PhoneRequestDto request){
-        if( token == null ){
+    public PhoneResponseDto changePhone(@RequestHeader(name = "Authorization", required = false) String token, PhoneRequestDto request) {
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
         TokenInfo tokenInfo = JWTUtils.getTokenInfo(token);
         Integer accountId = tokenInfo.getAccountId();
-        return accountService.changePhone(accountId,request.getNewPhoneNum());
+        return accountService.changePhone(accountId, request.getNewPhoneNum());
     }
+
     //用户注销
     @DeleteMapping("/deleteUser")
-    public ResponseEntity<Void> deleteUser(@RequestHeader(name = "Authorization", required = false)String token){
-        if( token == null ){
+    public ResponseEntity<Void> deleteUser(@RequestHeader(name = "Authorization", required = false) String token) {
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
@@ -209,14 +226,14 @@ public class AccountController {
     }
 
     @PostMapping("/prePaid")
-    public ResponseEntity<Void> prepaid(@RequestHeader(name = "Authorization", required = false)String token,  Double money){
-        if( token == null ){
+    public ResponseEntity<Void> prepaid(@RequestHeader(name = "Authorization", required = false) String token, Double money) {
+        if (token == null) {
             throw new NotFoundException("token is null");
         }
         token = token.substring(7);
         TokenInfo tokenInfo = JWTUtils.getTokenInfo(token);
         Integer accountId = tokenInfo.getAccountId();
 
-        return accountService.creditAccount(accountId,money);
+        return accountService.creditAccount(accountId, money);
     }
 }
